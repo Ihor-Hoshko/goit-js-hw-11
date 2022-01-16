5664775b526345de9b211bc54a0f560a658991c8
Ihor-Hoshko <fata77698@gmail.com>
2022-01-16 18:58:26 +0200

    123

--- src/srv.js
+++ src/srv.js
@@ -1,4 +1,50 @@
 import axios from 'axios';
 
-const apiKey = '25272206 - 904bc923b3419c336cabf17c5';
+const API_KEY = '25272206 - 904bc923b3419c336cabf17c5';
 axios.defaults.baseURL = 'https://pixabay.com/api/';
+
+const fetchSearch = async (data, page) => {
+  const result = await axios.get(
+    `?key=${API_KEY}&q=${data}&imageType=photo&orientation=horizontal&safesearch=true&per_page=40&page=${page}`,
+  );
+  const { hits, totalHits } = result.data;
+
+  return { hits, totalHits };
+};
+
+const createMarkup = images => {
+  const markup = images
+    .map(
+      ({ webformatURL, tags, likes, views, comments, downloads, previewWidth, largeImageURL }) => {
+        return ` <div class="gallery">
+  <a href="largeImageURL"
+    ><img src="webformatURL" alt="" title="" />
+    <div
+      class="photo-card"
+      style="border: 1px solid #009688; border-radius: 4px; margin-bottom: 5px"
+    >
+      <img src="${webformatURL}" alt="${tags}" loading="lazy" width="260px" height="150px" />
+      <div class="info">
+        <p class="info-item">
+          <b> Likes: ${likes}</b>
+        </p>
+        <p class="info-item">
+          <b> Views: ${views}</b>
+        </p>
+        <p class="info-item">
+          <b> Comments: ${comments}</b>
+        </p>
+        <p class="info-item">
+          <b> Downloads: ${downloads}</b>
+        </p>
+      </div>
+    </div></a
+  >
+</div>`;
+      },
+    )
+    .join('');
+  return markup;
+};
+
+export { fetchSearch, createMarkup };
